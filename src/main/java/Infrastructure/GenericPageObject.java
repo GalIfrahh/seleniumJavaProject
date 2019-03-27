eccf2a967231dd58c2da5720961fb32e0d479712
GalIfrahh <48998366+GalIfrahh@users.noreply.github.com>
2019-03-28 00:30:07 +0200

    Update GenericPageObject.java

--- src/main/java/Infrastructure/GenericPageObject.java
+++ src/main/java/Infrastructure/GenericPageObject.java
@@ -2,25 +2,15 @@ package Infrastructure;
 
 public class GenericPageObject {
 	
-	
-	
-
 
 	public static WebDriverWrapper driverWrapper;
 	
-	
-	
-
 
 	public static void setDriver(WebDriverWrapper _driverWrapper) {
 		
-		
-		
-		
+				
 		driverWrapper = _driverWrapper;
 		
-	
-
 	}
 
 
