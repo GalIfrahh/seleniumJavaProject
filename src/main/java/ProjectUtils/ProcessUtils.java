9ea74e6dc99e67de2d0a68a3ac56137e751d0ae7
GalIfrahh <48998366+GalIfrahh@users.noreply.github.com>
2019-03-28 00:44:37 +0200

    Update ProcessUtils.java

--- src/main/java/ProjectUtils/ProcessUtils.java
+++ src/main/java/ProjectUtils/ProcessUtils.java
@@ -1,122 +1,71 @@
 package ProjectUtils;
 
-
-
 import java.io.IOException;
-
-
 
 
 	public class ProcessUtils {
 
 
-	
-
 		public static Process process;
 
-
-
 	
-	public static Runtime runtime = Runtime.getRuntime();
+		public static Runtime runtime = Runtime.getRuntime();
 
 	
 	
-
-
 		public static void startBatchFromCmd(String batchFilePath) {
-		
-		
-
 
 			try{    
 
-				
 
-				process = runtime.exec("cmd /c start " + batchFilePath);
-	 
-	 
-				
+				process = runtime.exec("cmd /c start " + batchFilePath);				
 
+				
 				process.waitFor();
-
-
-		
-	}
+				
 
 			catch(IOException e){
 
 
 				System.out.println(e.getMessage());
 
-
-
-
-	
+				
 		}
 
 			catch(InterruptedException e){
-
-
-	
+				
+				
 			System.out.println(ex.getMessage());
-
-		
 
 			}
 		 
- 
-
 		}
 
 
-	
 
 		public static void killProcess(){
-
-
 	
 		process.destroy();
 		
-	
-
 		}
 
 
-	
-	
 
 		public static void closeCmd(){
 
-	
-
 			try {
 		
-
-
 				runtime.exec("taskkill /im cmd.exe /t /f");
 		
-		
-
-
-			    } 
+			} 
 			
 			catch (IOException e){
 
-
-	
+				
 			System.out.println("io exception");
 				
-		
-
-				}
-
-
-		
+			}
+			
 	}
 
-
-	
-
-
-
-		}
+}
