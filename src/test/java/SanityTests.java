52dac648b45f7cb2c7eb5c93b87fbbbd459c0075
GalIfrahh <48998366+GalIfrahh@users.noreply.github.com>
2019-03-30 11:33:19 +0300

    Update SanityTests.java

--- src/test/java/SanityTests.java
+++ src/test/java/SanityTests.java
@@ -1,69 +1,68 @@
-import static org.junit.Assert.assertEquals;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import PageObjects.PageObjects.HomeScreen;
+import PageObjects.PageObjects.*;
+
+import static org.junit.Assert.*;
+
+public class SanityTests extends Basic {
 
 
+    public String time_stamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 
-public class ConnectTests extends Basic {
+
+    @Test
+    public void redLionSanity(){
+
+        HomePage.openRedLion();
+
+        ConnectPage.clickOnHelloRewards_connectBtn();
+
+        ConnectPage.enterEmail();
+
+        ConnectPage.enterPassword();
+
+        ConnectPage.clickOnSignIn();
+
+        HomePage.enterLocationName();
+
+        HomePage.chooseFirstLocatio();
+
+        HomePage.clickOnStartDateField();
+
+        int active_date = HomePage.getSelectedStartDateIndex(HomePage.getDatePickerValues());
+
+        HomePage.getDatePickerValues().get(active_date + 7).click();
+
+        HomePage.getDatePickerValues().get(active_date + 8).click();
+
+        HomePage.clickOnCheckAvailability();
+
+        HomePage.clickOnQuickBook();
+
+        String full_cancellation_text = OrderSummeryPage.getCancellationPolicyText();
+
+        if(OrderSummeryPage.HasCancellation(full_cancellation_text)) {
+
+            setTimeStemp(time_stamp);
+
+            driverWrapper.takeScreenShot(time_stamp);
+
+            // perform room reservation - needs real payment methods...
+
+            String confirmation_text = ConfirmationPage.getConfirmationText();
+
+            assertEquals(confirmation_text, prop.getProperty("CONFIRMATION_TEXT"));
+        }
 
 
-	public String time_stamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-	
-	
-	@Test
-	public void redLionSanity(){
-	
-		HomeScreen.openRedLion();
-			
-		ConnectPage.clickOnHelloRewards_connectBtn();
-				
-		ConnectPage.enterEmail();
-		
-		ConnectPage.enterPassword();
-		
-		ConnectPage.clickOnSignIn();
-		
-		HomePage.enterLocationName();
-		
-		HomePage.chooseFirstLocatio();
-	
-		HomePage.clickOnStartDateField();
-		
-		int active_date = HomeScreen.getSelectedStartDateIndex(HomeScreen.getDatePickerValues());
-		
-		HomePage.getDatePickerValues().get(active_date + 7).click();
-		
-		HomePage.getDatePickerValues().get(active_date + 8).click();
-		
-		HomePage.clickOnCheckAvailability();
-		
-		HomePage.clickOnQuickBook();
-				
-		String full_cancellation_text = OrderSummeryPage.getCencelationPolicyText();
-		
-		if(OrderSummeryPage.HasCancellation(full_cancellation_text)) {
-						
-			setTimeStemp(time_stamp);
-			
-			driverWrapper.takeScreenShot(time_stamp);
-			
-			// perform room reservation - needs real payments...
-		
-			confirmation_text = ConfirmationPage.getConfirmationText();
-			
-			assertEquals(confirmation_text, prop.getProperty('CONFIRMATION_TEXT'));
-			
-			
-		else{	
-			
-			fail("no cancellation policy");			
-		
-		}
-		
-		
-	     }
-				
-	}
+		else{
+
+                fail("no cancellation policy");
+
+            }
+
+
+        }
+
+}
