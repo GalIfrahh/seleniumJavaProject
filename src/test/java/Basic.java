5cb13bddd52c07b04622277f771b62795957d0cf
GalIfrahh <48998366+GalIfrahh@users.noreply.github.com>
2019-04-07 18:09:33 +0300

    Update Basic.java

--- src/test/java/Basic.java
+++ src/test/java/Basic.java
@@ -1,122 +1,121 @@
-
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.Properties;
 
+import ProjectUtils.ProcessUtils;
+import ProjectUtils.SlackService;
+
+import lombok.*;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.*;
-
 import com.mashape.unirest.http.exceptions.UnirestException;
-
 import Infrastructure.WebDriverWrapper;
 import Infrastructure.GenericPageObject;
-import ProjectUtils.SlackService;
-import ProjectUtils.ProcessUtils;
 import ProjectUtils.ReadPropertyFile;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Basic {
-	
-	
-	public WebDriverWrapper driverWrapper;
-	
-  	static final String HUB_BATCH_PATH = "\RedLionAutomation\Grid\startHub.bat";
 
-	static final String NODE_BATCH_PATH = "\RedLionAutomation\Grid\startNode.bat";
+
+	public WebDriverWrapper driverWrapper;
+
+	public static Properties prop = null;
 
 	public static ReadPropertyFile readProperties = new ReadPropertyFile();
-	
-	public static Properties prop;
-	
-	public static String time_stamp = null;
-	
-	
-	
+
+	public static ProcessUtils processUtils = new ProcessUtils();
+
+
+	@Setter @Getter public static String time_stamp = null;
+
+
+
+
 	@Rule
 	public TestName testName = new TestName();
-	
-	
+
+
+
 	@BeforeClass
 	public static void setupClass() {
-		
-		ProcessUtils.startBatchFromCmd(HUB_BATCH_PATH);
+
+			prop = new Properties();
 
 
-		ProcessUtils.startBatchFromCmd(NODE_BATCH_PATH);
-	
-
-		System.out.println("selenium Grid statred");
-		
-
-		prop = new Properties();
+			prop = readProperties.readPropFile(prop, "/home/gal/IdeaProjects/RedLionAutomation/src/test/java/SutProperties/config.properties");
 
 
-		prop = readProperties.readPropFile(prop, "\RedLionAutomation\src\test\java\SutProperties\config.properties");
-	
+			GenericPageObject.setProp(prop);
+
+
+			processUtils.startGrid(prop.getProperty("HUB_PATH"));
+
+
+			processUtils.startGrid(prop.getProperty("NODE_PATH"));
+
 	}
 
-	
+
+
 	@Before
 	public void setup() throws IOException {
-		
+
 		driverWrapper = new WebDriverWrapper();
 
-	
-		driverWrapper.init("http://localhost:4444/wd/hub");
-		
-		
-		driverWrapper.MaximizeWIndow();
-		
 
-		GenericPageObject.setDriver(driverWrapper);
-		
+		driverWrapper.init("http://localhost:4444/wd/hub");
+
+
+		driverWrapper.MaximizeWindow();
+
+
+		GenericPageObject.setDriverWrapper(driverWrapper);
+
+
 	}
-	
+
+
 
 	@After
 	public void tearDown() {
-		
+
 		System.out.println("end of: " + testName.getMethodName() + " end");
-		
-		
+
+
+
 		driverWrapper.close();
+
+
 	}
-	
+
+
 
 	@AfterClass
-
 	public static void tearDownClass() throws IOException, UnirestException {
-		
-
-		SlackService slack = new SlackService();
-		
-		
-		slack.uploadImage("\RedLionAutomation\ScreenShots\\" + time_stamp + ".png");
-		
-		
+//
+//		SlackService slack = new SlackService();
+//
+//
+//		slack.uploadImage("/home/gal/IdeaProjects/RedLionAutomation/ScreenShots/" + time_stamp + ".png");
+//
 
 		ProcessUtils.killProcess();
-		
-		
-		ProcessUtils.closeCmd();
-		
-		
+
+
 		readProperties.killInput();
 
-		
+
 		System.out.println("killed all running processes!");
-	
+
 	}
-	
-	
-	
-	public void setTimeStemp(String taken_time_stamp) {
-		
-		time_stamp = taken_time_stamp;
-	
-	}
-	
+
+
+
+
 }
